a9742bc55b97f6d218c93cb2d2643e44a54de346
sachinlegend <70750103+sachinlegend@users.noreply.github.com>
2021-08-01 17:53:49 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,13 @@
+var ground;
+var commando, commandoimg, commandoright, commandol;
+
+
+function setup() {
+  createCanvas(800,400);
+  createSprite(400, 200, 50, 50);
+}
+
+function draw() {
+  background(0);  
+  drawSprites();
+}
